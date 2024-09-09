372c63c5ad2c666d9f800869d63ff7dce7b8bbe7
Chris <ccolon29@mail.valenciacollege.edu>
2024-09-09 10:20:54 -0400

    added code to the form file that ensures persistence of the blogposts values in local storage when going between pages

--- Assets/js/blog.js
+++ Assets/js/blog.js
@@ -1,7 +1,30 @@
-const body = document.body;
-const h2El = document.createElement('h2');
-const liEl = document.createElement('li');
-const footerEl = document.createElement('footer');
+const blogEntries = document.querySelector('#blogEntries');
 
 const entries = [];
 
+function renderEntries() {
+    blogEntries.innerHTML = '';
+    
+    for (let i = 0; i < entries.length; i++) {
+        console.log(entries[i]);
+        const entry = entries[i];
+        console.log(entry[0].username);
+        const li = document.createElement('li');
+        li.textContent = `${entry[i].title}${entry[i].content}${entry[i].username}`;
+        li.setAttribute('data-index', i);
+
+        blogEntries.appendChild(li);
+    }
+};
+
+function init() {
+    const storedEntries = JSON.parse(localStorage.getItem('blogPost'));
+    console.log(storedEntries[0].content);
+    if (storedEntries !== null) {
+        entries.push(storedEntries);
+    }
+    console.log(entries);
+    renderEntries();
+};
+
+init();
--- Assets/js/form.js
+++ Assets/js/form.js
@@ -2,27 +2,36 @@ const usernameEl = document.getElementById("username");
 const titleEl = document.getElementById("title");
 const contentEl = document.getElementById("content");
 const blogFormEl = document.getElementById("blogForm");
-const msgDiv = document.getElementById("msg")
+const msgDiv = document.getElementById("msg");
+
+console.log(JSON.parse(localStorage.getItem('blogPost')));
+
+let posts = [];
+console.log(posts);
 
 function errorMsg(type, message) {
-    msgDiv.textContent = message;
-    msgDiv.setAttribute('class', type);
+  msgDiv.textContent = message;
+  msgDiv.setAttribute("class", type);
 }
-
 
 blogForm.addEventListener("submit", (event) => {
   event.preventDefault();
 
+  if (localStorage.getItem("blogPost") !== null) {
+    const storedPosts = (JSON.parse(localStorage.getItem('blogPost')));
+    posts = storedPosts;
+  }
+
   const titleValue = titleEl.value;
   const usernameValue = usernameEl.value;
   const contentValue = contentEl.value;
-  
+
   if (usernameValue === "") {
-    errorMsg('error', 'Username cannot be blank');
+    errorMsg("error", "Username cannot be blank");
   } else if (titleValue === "") {
-    errorMsg('error', 'Title cannot be blank');
+    errorMsg("error", "Title cannot be blank");
   } else if (contentValue === "") {
-    errorMsg('error', 'Content cannot be blank');
+    errorMsg("error", "Content cannot be blank");
   } else {
     const blogPost = {
       username: usernameValue,
@@ -30,7 +39,9 @@ blogForm.addEventListener("submit", (event) => {
       content: contentValue,
     };
 
-    localStorage.setItem("blogPost", JSON.stringify(blogPost));
+    posts.push(blogPost);
+
+    localStorage.setItem("blogPost", JSON.stringify(posts));
 
     window.location.assign("./blog.html");
   }
