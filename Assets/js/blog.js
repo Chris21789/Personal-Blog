33bcd6040c0b79cc911a89bc3c3d01fadd00afeb
Chris <ccolon29@mail.valenciacollege.edu>
2024-09-09 10:37:57 -0400

    added code to the blog file that properly renders the local storage posts into list items and appends them to the list on the blog page

--- Assets/js/blog.js
+++ Assets/js/blog.js
@@ -1,30 +1,17 @@
 const blogEntries = document.querySelector('#blogEntries');
 
-const entries = [];
+const entries = JSON.parse(localStorage.getItem('blogPost'));
 
 function renderEntries() {
     blogEntries.innerHTML = '';
     
     for (let i = 0; i < entries.length; i++) {
-        console.log(entries[i]);
-        const entry = entries[i];
-        console.log(entry[0].username);
         const li = document.createElement('li');
-        li.textContent = `${entry[i].title}${entry[i].content}${entry[i].username}`;
+        li.textContent = `${entries[i].title}${entries[i].content}${entries[i].username}`;
         li.setAttribute('data-index', i);
 
         blogEntries.appendChild(li);
     }
 };
 
-function init() {
-    const storedEntries = JSON.parse(localStorage.getItem('blogPost'));
-    console.log(storedEntries[0].content);
-    if (storedEntries !== null) {
-        entries.push(storedEntries);
-    }
-    console.log(entries);
-    renderEntries();
-};
-
-init();
+renderEntries();
